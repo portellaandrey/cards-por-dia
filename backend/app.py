6e19a2220f9f094d8c7b9d2e97741be63b3e4a8d
andrey portella <andreyportella.parana@gmail.com>
2026-02-10 22:48:18 -0300

    Dashboard Trello fullstack

--- backend/app.py
+++ backend/app.py
@@ -42,5 +42,7 @@ def cards_por_dia():
 
     return jsonify(contagem)
 
-if __name__ == "__main__":
-    app.run()
+    if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+
